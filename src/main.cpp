5a2ad3b6f1bd2e2b3831b2fc3083208e133c74e7
tjysdsg <tjy1018543509@163.com>
2019-02-05 16:15:35 +0800

    reflection

--- src/main.cpp
+++ src/main.cpp
@@ -27,12 +27,27 @@ __RAYTRACER_API__ bool scene_intersect(__in__ const Vec3f orig,
 }
 
 Vec3f emit_ray(const Vec3f &orig, const Vec3f &dir,
-               const std::vector<Sphere> spheres, std::vector<Light> lights) {
+               const std::vector<Sphere> spheres, std::vector<Light> lights,
+               size_t depth = 0) {
     Vec3f point, N;
     Material material;
-    if (!scene_intersect(orig, dir, spheres, point, N, material)) {
+    // if there is no reflection and intersection with other object, return the
+    // background color
+    if (depth > 4 || !scene_intersect(orig, dir, spheres, point, N, material)) {
         return Vec3f(0.2, 0.7, 0.8);  // background color
     }
+    // calculate reflection
+    Vec3f reflect_dir = reflect(dir, N).normalize();
+    Vec3f reflect_orig =
+        reflect_dir * N < 0
+            ? point - N * 1e-3
+            : point + N * 1e-3;  // offset the original point to avoid occlusion
+                                 // by the object itself
+    // emit ray recursively until depth is greater than
+    // TODO property reflection_depth
+    Vec3f reflect_color =
+        emit_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+
     // calculate light
     float diffuse_light_intensity = 0, specular_light_intensity = 0;
     for (size_t i = 0; i < lights.size(); i++) {
@@ -62,7 +77,8 @@ Vec3f emit_ray(const Vec3f &orig, const Vec3f &dir,
     }
     return material.diffuse_color * diffuse_light_intensity *
                material.albedo[0] +
-           Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1];
+           Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1] +
+           reflect_color * material.albedo[2];
 }
 
 int main() {
@@ -70,20 +86,23 @@ int main() {
     const int height = 768;
     std::vector<Vec3f> framebuffer(width * height);
     float fov = 3.1415926 / 4;
-    //
-    Material ivory(Vec2f(0.6, 0.3), Vec3f(0.4, 0.4, 0.3), 50.);
-    Material red_rubber(Vec2f(0.9, 0.1), Vec3f(0.3, 0.1, 0.1), 10.);
+    // add some materials
+    Material ivory(Vec3f(0.6, 0.3, 0.1), Vec3f(0.4, 0.4, 0.3), 50.);
+    Material red_rubber(Vec3f(0.9, 0.1, 0.0), Vec3f(0.3, 0.1, 0.1), 10.);
+    Material mirror(Vec3f(0.0, 10.0, 0.8), Vec3f(1.0, 1.0, 1.0), 1425.);
+    // add some spheres with material into the scene
     std::vector<Sphere> spheres;
     spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
-    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, mirror));
     spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
-    spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, ivory));
-    //
+    spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, mirror));
+    // add some light sources
     std::vector<Light> lights;
     lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
     lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
     lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
 
+    // render procedures
     for (size_t j = 0; j < height; j++) {
         for (size_t i = 0; i < width; i++) {
             float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) *
@@ -96,6 +115,7 @@ int main() {
             framebuffer[i + j * width] = c;
         }
     }
+    // save the result to file
     render2img("out.ppm", width, height, framebuffer);
     return 0;
 }
--- src/material.h
+++ src/material.h
@@ -4,11 +4,11 @@
 #include "geometry.h"
 struct Material {
     Vec3f diffuse_color;
-    Material(const Vec2f &a, const Vec3f &color, const float &spec)
+    Material(const Vec3f a, const Vec3f color, const float spec)
         : albedo(a), diffuse_color(color), specular_exponent(spec) {}
-    Material() : albedo(1, 0), diffuse_color(), specular_exponent() {}
+    Material() : albedo(1, 0, 0), diffuse_color(), specular_exponent() {}
     float specular_exponent;
-    Vec2f albedo;
+    Vec3f albedo;
 };
 
 #endif  // __MATERIAL_H__
