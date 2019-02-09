08a4bc92f858b9d414a00047ec47f0bb52542fa8
tjysdsg <tjy1018543509@163.com>
2019-02-09 16:16:39 +0800

    refraction

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,23 @@
 #include "material.h"
 #include "sphere.h"
 
-Vec3f reflect(const Vec3f &I, const Vec3f &N) { return I - N * 2.f * (I * N); }
+Vec3f reflect(const Vec3f I, const Vec3f N) { return I - N * 2.f * (I * N); }
+
+Vec3f refract(const Vec3f I, const Vec3f N,
+              const float refractive_index) {  // Snell's law
+    float cosi = -std::max(-1.f, std::min(1.f, I * N));
+    float etai = 1, etat = refractive_index;
+    Vec3f n = N;
+    if (cosi < 0) {  // if the ray is inside the object, swap the indices and
+                     // invert the normal to get the correct result
+        cosi = -cosi;
+        std::swap(etai, etat);
+        n = -N;
+    }
+    float eta = etai / etat;
+    float k = 1 - eta * eta * (1 - cosi * cosi);
+    return k < 0 ? Vec3f(0, 0, 0) : I * eta + n * (eta * cosi - sqrtf(k));
+}
 
 // TODO: refactor this rubbish API
 __RAYTRACER_API__ bool scene_intersect(__in__ const Vec3f orig,
@@ -46,9 +62,17 @@ Vec3f emit_ray(const Vec3f &orig, const Vec3f &dir,
             ? point - N * 1e-3
             : point + N * 1e-3;  // offset the original point to avoid occlusion
                                  // by the object itself
+
     // emit ray recursively until depth is greater than
     Vec3f reflect_color =
         emit_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+
+    // calculate refraction
+    Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
+    Vec3f refract_orig =
+        refract_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3;
+    Vec3f refract_color =
+        emit_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
 
     // calculate light
     float diffuse_light_intensity = 0, specular_light_intensity = 0;
@@ -80,7 +104,8 @@ Vec3f emit_ray(const Vec3f &orig, const Vec3f &dir,
     return material.diffuse_color * diffuse_light_intensity *
                material.albedo[0] +
            Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1] +
-           reflect_color * material.albedo[2];
+           reflect_color * material.albedo[2] +
+           refract_color * material.albedo[3];
 }
 
 int main() {
@@ -89,13 +114,17 @@ int main() {
     std::vector<Vec3f> framebuffer(width * height);
     float fov = 3.1415926 / 4;
     // add some materials
-    Material ivory(Vec3f(0.6, 0.3, 0.1), Vec3f(0.4, 0.4, 0.3), 50.);
-    Material red_rubber(Vec3f(0.9, 0.1, 0.0), Vec3f(0.3, 0.1, 0.1), 10.);
-    Material mirror(Vec3f(0.0, 10.0, 0.8), Vec3f(1.0, 1.0, 1.0), 1425.);
+    Material ivory(1.0, Vec4f(0.6, 0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3), 50.);
+    Material glass(1.5, Vec4f(0.0, 0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8), 125.);
+    Material red_rubber(1.0, Vec4f(0.9, 0.1, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1),
+                        10.);
+    Material mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0),
+                    1425.);
+
     // add some spheres with material into the scene
     std::vector<Sphere> spheres;
     spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
-    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, mirror));
+    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, glass));
     spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
     spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, mirror));
     // add some light sources
--- src/material.h
+++ src/material.h
@@ -4,11 +4,19 @@
 #include "geometry.h"
 struct Material {
     Vec3f diffuse_color;
-    Material(const Vec3f a, const Vec3f color, const float spec)
-        : albedo(a), diffuse_color(color), specular_exponent(spec) {}
-    Material() : albedo(1, 0, 0), diffuse_color(), specular_exponent() {}
+    Material(const float r, const Vec4f a, const Vec3f color, const float spec)
+        : refractive_index(r),
+          albedo(a),
+          diffuse_color(color),
+          specular_exponent(spec) {}
+    Material()
+        : refractive_index(1),
+          albedo(1, 0, 0, 0),
+          diffuse_color(),
+          specular_exponent() {}
     float specular_exponent;
-    Vec3f albedo;
+    float refractive_index;
+    Vec4f albedo;
 };
 
 #endif  // __MATERIAL_H__
